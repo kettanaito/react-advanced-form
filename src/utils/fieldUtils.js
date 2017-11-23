a8d2f655d75057f5d4948a525c819378d4672ae8
Artem Zakharchenko <kettanaito@gmail.com>
2017-11-23 17:07:00 +0100

    Field: Introduce "valid" and "invalid" props

--- src/utils/fieldUtils.js
+++ src/utils/fieldUtils.js
@@ -36,3 +36,25 @@ export function resolveProp({ propName, fieldProps, fields }) {
 export function shouldValidateField({ fieldProps }) {
   return !fieldProps.validated || (typeof fieldProps.required === 'function');
 }
+
+/**
+ * Returns whether the field is valid in the given context.
+ * Field cannot rely on "valid" prop alone. Consider this:
+ * - valid when it has value, has been validated and is indeed valid
+ * - invalid - when it has been validated, but it's not valid
+ */
+export function updateValidState({ name, value, validated, expected }) {
+  const validState = {
+    valid: !!value && validated && expected,
+    invalid: validated && !expected
+  };
+
+  console.groupCollapsed(name, '@ updateValidState');
+  console.log('value', value);
+  console.log('validated', validated);
+  console.log('expected', expected);
+  console.log('validState', validState);
+  console.groupEnd();
+
+  return validState;
+}
