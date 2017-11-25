25f4054331459c75db6c97f84c7e594921baca55
Artem Zakharchenko <kettanaito@gmail.com>
2017-11-25 17:28:22 +0100

    Stories: Add RFQ example

--- stories/index.js
+++ stories/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import DefaultForm from './DefaultForm';
+import RfqForm from './RfqForm';
 
 storiesOf('Form', module)
-  .add('Default form', () => (<DefaultForm />));
+  .add('Default form', () => (<DefaultForm />))
+  .add('RFQ form', () => <RfqForm />);
