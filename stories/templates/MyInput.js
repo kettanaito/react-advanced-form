389012c089026eab1e817701cea7cd651d8ed134
Artem Zakharchenko <kettanaito@gmail.com>
2017-11-25 19:11:23 +0100

    Playing with custom icons

--- stories/templates/MyInput.js
+++ stories/templates/MyInput.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Field, connectField } from '../../src';
+import feather from 'feather-icons';
 
 const inputStyles = {
   border: '1px solid #ccc',
-  borderRadius: '3px',
+  borderRadius: 3,
   margin: 0,
   padding: '.45rem .65rem',
   fontSize: '14px'
@@ -11,19 +12,29 @@ const inputStyles = {
 
 class MyCustomInput extends React.Component {
   render() {
-    const { valid, invalid, ...restProps } = this.props;
+    const { valid, invalid, error, ...restProps } = this.props;
+
+    const iconOptions = { width: 16, height: 16 };
+
+    const borderColor = (valid && 'green') || (invalid && '#cc0000');
+    const iconColor = (valid && 'green') || (invalid && '#cc0000');
+    const icon = (valid && feather.icons.check.toSvg(iconOptions)) || (invalid && feather.icons['alert-circle'].toSvg(iconOptions));
 
     return (
       <div className="form-group" style={{ marginBottom: '1rem' }}>
-        <div style={{ display: 'flex' }}>
-          <Field.Input {...this.props} style={ inputStyles } />
-          { valid && (
-            <span style={{ color: 'green', marginTop: '6px' }}>✓</span>
+        <div style={{ display: 'inline-flex', position: 'relative' }}>
+          <Field.Input {...this.props} style={{ ...inputStyles, borderColor }} />
+          { (valid || invalid) && (
+            <span
+              style={{ position: 'absolute', height: 16, color: iconColor, top: 0, bottom: 0, margin: 'auto', right: 8 }}
+              dangerouslySetInnerHTML={{
+                __html: icon
+              }} />
           ) }
         </div>
 
         { invalid && (
-          <p style={{ color: 'red', marginTop: '4px' }}>Please provide a proper value.</p>
+          <p style={{ color: '#cc0000', marginTop: 4 }}>Error: { error }</p>
         ) }
       </div>
     );
