c923a90cabb30f0a01f761bd918d229d607f9f57
Artem Zakharchenko <kettanaito@gmail.com>
2017-11-29 18:40:33 +0100

    utils (IterableInstance): Use invariant; Improve the error message

--- src/utils/IterableInstance.js
+++ src/utils/IterableInstance.js
@@ -1,13 +1,13 @@
 /**
  * Custom iterable prop type validator.
+ * This is needed since "PropTypes.instanceOf(Immutable.Map)" doesn't resolve to "true".
+ * Multiple bugs are issued relatively to that, hence there is a custom validation.
  */
+import invariant from 'invariant';
 import { Iterable } from 'immutable';
 
 export default function IterableInstance(props, propName, componentName) {
-  const isValid = Iterable.isIterable(props[propName]);
+  const propValue = props[propName];
 
-  if (!isValid) {
-    return new Error(`Invalid prop "${propName}" supplied to "${componentName},
-      expected an instance of Iterable (ImmutableJS).`);
-  }
+  invariant(Iterable.isIterable(propValue), `Invalid prop \`${propName}\` of type \`${typeof propValue}\` supplied to \`${componentName}\`, expected an instance of Iterable.`);
 }
