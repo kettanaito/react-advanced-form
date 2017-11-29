b6415f64f0f3d8679f1f3c198c2155d1bc7a74c9
Artem Zakharchenko <kettanaito@gmail.com>
2017-11-29 18:54:15 +0100

    fieldUtils (getDynamicProps): Provide better descriptions, type comments

--- src/utils/fieldUtils/getDynamicProps.js
+++ src/utils/fieldUtils/getDynamicProps.js
@@ -1,8 +1,11 @@
-/**
- * Write me
- */
+/* The list of all props which may be dynamic */
 export const dynamicProps = ['required', 'disabled'];
 
+/**
+ * Returns the collection of the present dynamic props in the provided field.
+ * @param {Map} fieldProps
+ * @return {object}
+ */
 export default function getDynamicProps(fieldProps) {
   return dynamicProps.reduce((props, dynamicProp) => {
     const propValue = fieldProps[dynamicProp];
