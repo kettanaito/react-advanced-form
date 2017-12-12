c997e07919495f81e52010429f7207989f72cbf3
Artem Zakharchenko <kettanaito@gmail.com>
2017-12-12 18:23:24 +0100

    Utils: Add "capitalize" method

---
+++ src/utils/capitalize.js
@@ -0,0 +1,6 @@
+/**
+ * Shorthand method of capitalizing the given string.
+ */
+export default function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
