8d620aaf2599ef932d9d6173b1bfb5a2b485290f
Artem Zakharchenko <kettanaito@gmail.com>
2017-12-13 21:04:54 +0100

    Test (Form): Add "serialize" method test

--- test/unit/components/Form.spec.js
+++ test/unit/components/Form.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
-import { FormProvider, Form } from '../../../lib';
+import { FormProvider, Form, Field } from '../../../lib';
 import { validationRules, validationMessages } from '../../common';
 
 describe('Form', () => {
@@ -26,5 +26,28 @@ describe('Form', () => {
 
     expect(rules.toJS()).to.deep.equal(validationRules);
     expect(messages.toJS()).to.deep.equal(validationMessages);
+  });
+
+  it('Can be serialized manually', () => {
+    const wrapper = mount(
+      <Form rules={ validationRules } messages={ validationMessages }>
+        <Field.Input name="username" value="doe" />
+        <Field.Group name="primaryInfo">
+          <Field.Input name="username" value="foo" />
+        </Field.Group>
+      </Form>
+    );
+
+    setTimeout(() => {
+      const { serialize } = wrapper.find(Form).instance();
+
+      expect(serialize).not.to.be.undefined;
+      expect(serialize()).to.deep.equal({
+        username: 'doe',
+        primaryInfo: {
+          username: 'foo'
+        }
+      });
+    }, 0);
   });
 });
