45e8d294770c9d0da3ed422304b46dc8b47f8ede
Artem Zakharchenko <kettanaito@gmail.com>
2017-12-13 11:24:12 +0100

    Test: Add "common" with validation rules and messages

---
+++ test/common/validationMessages.js
@@ -0,0 +1,17 @@
+export default {
+  general: {
+    missing: 'Please provide the required field.',
+    invalid: 'Please provide a proper value.'
+  },
+  type: {
+    tel: {
+      invalid: 'Please provide a proper phone number.'
+    }
+  },
+  name: {
+    username: {
+      missing: 'Please provide a proper username.',
+      invalid: 'User does not exist.'
+    }
+  }
+};
