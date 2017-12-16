ed3496a3aff5e2e18f6db5c801ba08d69c202cb2
Artem Zakharchenko <kettanaito@gmail.com>
2017-12-16 18:57:43 +0100

    Test (connectField): Add test for Field.Group over custom field component

--- test/unit/components/connectField.spec.js
+++ test/unit/components/connectField.spec.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
+import { Map } from 'immutable';
 import { connectField, Form, Field } from '../../../lib';
 import { defer } from '../../common';
 
 describe('connectField', () => {
-  it('Wrapped field is registered with proper "fieldPath"', (done) => {
+  it('Wrapped field is registered with correct "fieldPath"', (done) => {
     const CustomField = props => (
       <div>
         <Field.Input {...props} />
@@ -24,7 +25,7 @@ describe('connectField', () => {
       const form = wrapper.find(Form).instance();
       expect(form.state.fields.has('username')).to.be.true;
 
-      done();
+      return done();
     });
   });
 
@@ -62,7 +63,36 @@ describe('connectField', () => {
         'error'
       ]);
 
-      done();
+      return done();
+    });
+  });
+
+  it('Wrapped field within Field.Group registers with the correct "fieldPath"', (done) => {
+    const CustomField = props => (
+      <div>
+        <Field.Input {...props} />
+      </div>
+    );
+
+    const WrappedField = connectField(CustomField);
+
+    const wrapper = mount(
+      <Form>
+        <Field.Group name="primaryInfo">
+          <WrappedField name="username" required />
+        </Field.Group>
+      </Form>
+    );
+
+    defer(() => {
+      const form = wrapper.find(Form).instance();
+      const groupedField = form.state.fields.get('primaryInfo.username');
+
+      expect(groupedField).to.be.instanceOf(Map);
+      expect(groupedField.get('name')).to.equal('username');
+      expect(groupedField.get('fieldPath')).to.equal('primaryInfo.username');
+
+      return done();
     });
   });
 });
