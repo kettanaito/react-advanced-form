f11e2de89ac62768ae43bc1480f7d1f68ec3ca67
Artem Zakharchenko <kettanaito@gmail.com>
2017-12-19 17:20:38 +0100

    fieldUtils (shouldValidate): Delegate an extensive logic

--- src/utils/fieldUtils/shouldValidate.js
+++ src/utils/fieldUtils/shouldValidate.js
@@ -1,19 +1,33 @@
 /**
  * Determines whether a Field with the provided props should be validated.
  * @param {Map} fieldProps
+ * @param {Map} formRules
  */
-export default function shouldValidateField(fieldProps) {
+export default function shouldValidateField({ fieldProps, formRules }) {
+  const value = fieldProps.get('value');
   const validatedSync = fieldProps.get('validatedSync');
   const required = fieldProps.get('required');
 
+  /* Field with dynamic "required" props are always to validate */
   const hasDynamicRequired = (typeof required === 'function');
-  const shouldValidate = hasDynamicRequired || (!validatedSync && required);
+  if (hasDynamicRequired) return true;
 
-  // console.groupCollapsed('fieldUtils @ shouldValidateField');
-  // console.log('was validated before (sync):', validatedSync);
-  // console.log('"required" prop is dynamic (func):', hasDynamicRequired);
-  // console.log('should validate:', shouldValidate);
-  // console.groupEnd();
+  /* Bypass the fields which are already validated sync */
+  if (validatedSync) return false;
+
+  /* Always validate required fields */
+  if (required) return true;
+
+  /* Validate optional non-empty fields with rules */
+  const hasValue = !!value;
+  const hasNameRule = formRules.hasIn(['name', fieldProps.get('name')]);
+
+  if (hasValue && hasNameRule) return true;
+
+  const hasTypeRule = formRules.hasIn(['type', fieldProps.get('type')]);
+  if (hasValue && hasTypeRule) return true;
+
+  return false;
 
   return shouldValidate;
 }
