3a1c2b4aa659d8d7d852e99a318a95afd3756198
Nadia Ridko <nadia.ridko@gmail.com>
2018-01-03 17:16:56 +0100

    Fields: Simplify "mapPropsToField" declarations

--- src/Fields/Checkbox.jsx
+++ src/Fields/Checkbox.jsx
@@ -9,7 +9,6 @@ export default createField({
   valueProp: 'checked',
   mapPropsToField: props => ({
     ...props,
-    checked: props.checked,
     initialValue: props.checked
   }),
   enforceProps: (props, contextProps) => ({
@@ -17,62 +16,3 @@ export default createField({
     checked: contextProps.get('checked')
   })
 })(Checkbox);
-
-// export default class Checkbox extends Field {
-//   static displayName = 'Field.Checkbox'
-
-//   static propTypes = {
-//     /* General */
-//     id: PropTypes.string,
-//     className: PropTypes.string,
-
-//     /* Specific */
-//     name: PropTypes.string.isRequired,
-//     checked: PropTypes.bool,
-//     disabled: PropTypes.bool,
-//     required: PropTypes.bool,
-
-//     /* Event handlers */
-//     onFocus: PropTypes.func,
-//     onChange: PropTypes.func,
-//     onBlur: PropTypes.func
-//   }
-
-//   static defaultProps = {
-//     expected: true,
-//     checked: false,
-//     disabled: false,
-//     required: false
-//   }
-
-//   fieldWillRegister() {
-//     const { checked } = this.props;
-
-//     return {
-//       ...this.props,
-//       type: 'checkbox',
-//       checked,
-//       initialValue: checked
-//     };
-//   }
-
-//   handleChange = (event) => {
-//     const { contextProps } = this;
-//     const { checked: nextValue } = event.currentTarget;
-//     const prevValue = contextProps.get('checked');
-
-//     return this.context.handleFieldChange({
-//       event,
-//       fieldProps: contextProps,
-//       valueProp: 'checked',
-//       prevValue,
-//       nextValue
-//     });
-//   }
-
-//   renderElement(props, contextProps) {
-//     return (
-//       <input checked={ contextProps.get('checked') } />
-//     );
-//   }
-// }
--- src/Fields/Select.jsx
+++ src/Fields/Select.jsx
@@ -1,64 +1,21 @@
 import React from 'react';
 import createField from '../createField';
 
-class Select extends React.Component {
+function Select({ children, ...props }) {
+  return (
+    <select { ...props }>
+      { children }
+    </select>
+  );
+}
+
+export default createField({
   /**
-   * Field registration.
    * This is a Field's lifecycle method called immediately before its registration in the Form.
    * The Object this method returns is treated as field props to be registered in the Form.
    */
-  fieldWillRegister() {
-    console.warn('SELECT CALLS FIELD WILL REGISTER!');
-    
-    const { children, initialValue } = this.props;
-    const firstChildValue = children && children[0].value;
-
-    return {
-      ...this.props,
-      initialValue: initialValue || firstChildValue
-    };
-  }
-
-  render() {
-    return (
-      <select {...this.props}>
-        { this.props.children }
-      </select>
-    );
-  }
-}
-
-export default createField()(Select);
-
-// export default class Select extends Field {
-//   static displayName = 'Field.Select'
-
-//   static propTypes = {
-//     id: PropTypes.string,
-//     className: PropTypes.string,
-//     value: PropTypes.string
-//   }
-
-//   /**
-//    * Field registration.
-//    * This is a Field's lifecycle method called immediately before its registration in the Form.
-//    * The Object this method returns is treated as field props to be registered in the Form.
-//    */
-//   fieldWillRegister() {
-//     const { children, initialValue } = this.props;
-//     const firstChildValue = children && children[0].value;
-
-//     return {
-//       ...this.props,
-//       initialValue: initialValue || firstChildValue
-//     };
-//   }
-
-//   renderElement(props) {
-//     return (
-//       <select>
-//         { props.children }
-//       </select>
-//     );
-//   }
-// }
+  mapPropsToField: ({ children, initialValue, ...props }) => ({
+    ...props,
+    initialValue: initialValue || (children && children[0].value)
+  })
+})(Select);
