7696fc12c20d48e9ac31a65679558b931767862a
Artem Zakharchenko <kettanaito@gmail.com>
2018-01-22 09:43:01 +0100

    WIP Simplify form schema validation

--- test/unit/utils/validateSync.spec.js
+++ test/unit/utils/validateSync.spec.js
@@ -13,254 +13,285 @@ describe('validateSync', () => {
     })
   });
 
-  const formRules = fromJS({
-    type: {
-      tel: ({ value }) => /^\d+$/.test(value)
-    },
-    name: {
-      username: ({ value }) => (value === 'admin')
-    }
-  });
-
-  it('empty optional field is expected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        value: '',
-        required: false
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: true
+  it('Validates by form type rules', () => {
+    const formRules = fromJS({
+      type: {
+        email: ({ value }) => (value === '123'),
+        password: {
+          minLength: ({ value }) => (value.length > 5),
+          capitalLetter: ({ value }) => value.test(/[A-Z]/g)
+        }
+      }
     });
-  });
 
-  it('empty required fields is unexpected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        value: '',
-        required: true
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
+    const firstField = Map({ name: 'a1', type: 'email', value: '456' });
+    const secondField = Map({ name: 'a2', type: 'password', value: 'sOme' });
+
+    expect(fieldUtils.validateSync({ fieldProps: firstField, fields, formRules })).to.deep.equal({
       expected: false,
-      errorType: 'missing'
+      errorType: ['invalid']
+    });
+
+    expect(fieldUtils.validateSync({ fieldProps: secondField, fields, formRules })).to.deep.equal({
+      expect: false,
+      errorType: ['minLength']
     });
   });
 
-  it('filled required field is expected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        value: 'foo',
-        required: true
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: true
-    });
-  });
+  /**
+   * ====================================================================================
+   */
 
-  it('optional empty field with rule is expected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        value: '',
-        rule: /^\d+$/,
-        required: false
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: true
-    });
-  });
+  // const formRules = fromJS({
+  //   type: {
+  //     tel: ({ value }) => /^\d+$/.test(value)
+  //   },
+  //   name: {
+  //     username: {
+  //       isAdmin: ({ value }) => (value === 'admin')
+  //     }
+  //   }
+  // });
 
-  it('optional field with rule (not matching) is unexepcted', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        value: 'foo',
-        rule: /^\d+$/,
-        required: false
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: false,
-      errorType: 'invalid'
-    });
-  });
+  // it('empty optional field is expected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       value: '',
+  //       required: false
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: true
+  //   });
+  // });
 
-  it('empty require field with rule is unexpected', () => {
-    /* Required empty value with rule */
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        value: '',
-        rule: /^\d+$/,
-        required: true
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: false,
-      errorType: 'missing'
-    });
-  });
+  // it('empty required fields is unexpected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       value: '',
+  //       required: true
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: false,
+  //     errorType: ['missing']
+  //   });
+  // });
 
-  it('filled required field with rule (not matching) is unexpected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        value: 'foo',
-        rule: /^\d+$/,
-        required: true
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: false,
-      errorType: 'invalid'
-    });
-  });
+  // it('filled required field is expected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       value: 'foo',
+  //       required: true
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: true
+  //   });
+  // });
 
-  it('filled required field with rule (matching) is expected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        value: '123',
-        rule: /^\d+$/,
-        required: true
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: true
-    });
-  });
+  // it('optional empty field with rule is expected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       value: '',
+  //       rule: /^\d+$/,
+  //       required: false
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: true
+  //   });
+  // });
 
-  it('filled optional field with rule (matching) is expected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        value: '123',
-        rule: /^\d+$/,
-        required: false
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: true
-    });
-  });
+  // it('optional field with rule (not matching) is unexepcted', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       value: 'foo',
+  //       rule: /^\d+$/,
+  //       required: false
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: false,
+  //     errorType: ['invalid']
+  //   });
+  // });
 
-  it('empty optional field with custom rule resolver is expected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        value: '',
-        rule: ({ fields }) => (fields.anotherField.value === 'foo'),
-        required: false
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: true
-    });
-  });
+  // it('empty require field with rule is unexpected', () => {
+  //   /* Required empty value with rule */
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       value: '',
+  //       rule: /^\d+$/,
+  //       required: true
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: false,
+  //     errorType: ['missing']
+  //   });
+  // });
 
-  it('filled optional field with custom rule resolver (matching) is expected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        value: '123',
-        rule: ({ fields }) => (fields.anotherField.value === 'foo'),
-        required: false
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: true
-    });
-  });
+  // it('filled required field with rule (not matching) is unexpected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       value: 'foo',
+  //       rule: /^\d+$/,
+  //       required: true
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: false,
+  //     errorType: ['invalid']
+  //   });
+  // });
 
-  it('empty optional field (form rule)is expected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        type: 'tel',
-        value: '',
-        required: false
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: true
-    });
-  });
+  // it('filled required field with rule (matching) is expected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       value: '123',
+  //       rule: /^\d+$/,
+  //       required: true
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: true
+  //   });
+  // });
 
-  it('filled optional field (form rule not matching) is unexpected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        type: 'tel',
-        value: 'abc',
-        required: false
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: false,
-      errorType: 'invalid'
-    });
-  });
+  // it('filled optional field with rule (matching) is expected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       value: '123',
+  //       rule: /^\d+$/,
+  //       required: false
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: true
+  //   });
+  // });
 
-  it('filled required field (form rule matching) is expected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        type: 'tel',
-        value: '123',
-        required: true
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: true
-    });
-  });
+  // it('empty optional field with custom rule resolver is expected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       value: '',
+  //       rule: ({ fields }) => (fields.anotherField.value === 'foo'),
+  //       required: false
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: true
+  //   });
+  // });
 
-  it('optional filled field (form rule not matching) is unexpected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        name: 'username',
-        value: 'foo',
-        required: false
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: false,
-      errorType: 'invalid'
-    });
-  });
+  // it('filled optional field with custom rule resolver (matching) is expected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       value: '123',
+  //       rule: ({ fields }) => (fields.anotherField.value === 'foo'),
+  //       required: false
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: true
+  //   });
+  // });
 
-  it('filled optional field (form rule matching) is expected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        name: 'username',
-        value: 'admin',
-        required: false
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: true
-    });
-  });
+  // it('empty optional field (form rule)is expected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       type: 'tel',
+  //       value: '',
+  //       required: false
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: true
+  //   });
+  // });
 
-  it('filled required field (form rule matching) is expected', () => {
-    expect(fieldUtils.validateSync({
-      fieldProps: Map({
-        name: 'username',
-        value: 'admin',
-        required: true
-      }),
-      fields,
-      formRules
-    })).to.deep.eq({
-      expected: true
-    });
-  });
+  // it('filled optional field (form rule not matching) is unexpected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       type: 'tel',
+  //       value: 'abc',
+  //       required: false
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: false,
+  //     errorType: ['invalid']
+  //   });
+  // });
+
+  // it('filled required field (form rule matching) is expected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       type: 'tel',
+  //       value: '123',
+  //       required: true
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: true
+  //   });
+  // });
+
+  // it('optional filled field (form rule not matching) is unexpected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       name: 'username',
+  //       value: 'foo',
+  //       required: false
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: false,
+  //     errorType: ['invalid']
+  //   });
+  // });
+
+  // it('filled optional field (form rule matching) is expected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       name: 'username',
+  //       value: 'admin',
+  //       required: false
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: true
+  //   });
+  // });
+
+  // it('filled required field (form rule matching) is expected', () => {
+  //   expect(fieldUtils.validateSync({
+  //     fieldProps: Map({
+  //       name: 'username',
+  //       value: 'admin',
+  //       required: true
+  //     }),
+  //     fields,
+  //     formRules
+  //   })).to.deep.eq({
+  //     expected: true
+  //   });
+  // });
 });
