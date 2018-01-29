a6da2eae1ed44e7525eada24e0fe322ba01da7c5
Artem Zakharchenko <kettanaito@gmail.com>
2018-01-29 13:30:23 +0100

    utils: Add separated "isset" file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-export FormProvider from './components/FormProvider';
+/* High-order components */
 export createField from './components/createField';
+
+/* Components */
+export FormProvider from './components/FormProvider';
 export Form from './components/Form';
-export * as Field from './components/Field';
 export Condition from './components/Condition';
+export * as Field from './components/Field';
 
 /* Field presets */
 export * as fieldPresets from './fieldPresets';
