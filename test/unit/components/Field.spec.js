8e26b4a4c1d98711d021b86b608e8f3a5f2f1815
Artem Zakharchenko <kettanaito@gmail.com>
2018-02-02 12:49:42 +0100

    Form (validateField): Ensure no next validity state is not requested when no validation needed

--- test/unit/components/Field.spec.js
+++ test/unit/components/Field.spec.js
@@ -1,9 +1,11 @@
+import { fromJS } from 'immutable';
 import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
 import { Input } from '../../components';
 import { defer } from '../../common';
 import { Form } from '../../../lib';
+import { fieldUtils } from '../../../src/utils';
 
 describe('Field', function () {
   it('Allows undefined "initialValue"', () => {
@@ -30,6 +32,35 @@ describe('Field', function () {
 
       expect(input).to.not.be.undefined;
       expect(input.get('initialValue')).to.equal(undefined);
+    });
+  });
+
+  it('Validity state update is omitted for validation-free fields', () => {
+    const rules = fromJS({
+      name: {
+        someName: ({ value }) => (value !== '123')
+      }
+    });
+
+    const wrapper = mount(
+      <Form rules={ rules }>
+        <Input name="foo" value="123" />
+      </Form>
+    );
+
+    return defer(async () => {
+      const form = wrapper.find(Form).instance();
+      const prevFieldProps = form.state.fields.get('foo');
+      const prevValidityState = fieldUtils.getValidityState(prevFieldProps);
+
+      await form.validate();
+
+      const nextFieldProps = form.state.fields.get('foo');
+      const nextValidityState = fieldUtils.getValidityState(nextFieldProps);
+
+      expect(nextFieldProps.get('validatedSync')).to.be.false;
+      expect(nextFieldProps.get('validatedAsync')).to.be.false;
+      expect(nextValidityState.equals(prevValidityState));
     });
   });
 });
