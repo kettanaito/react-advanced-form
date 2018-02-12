a21723ac1f6eb325ed17409f8d5bb2a14ae2afac
Artem Zakharchenko <kettanaito@gmail.com>
2018-02-12 22:39:03 +0100

    utils (warn): Add JSDocs

--- src/utils/warn.js
+++ src/utils/warn.js
@@ -1,3 +1,9 @@
+/**
+ * Calls `console.warn` with the provided message when the `testValue` rejects.
+ * @param {boolean} testValue
+ * @param {string} message
+ * @param {any[]} optionalParams
+ */
 export default function warn(testValue, message, ...optionalParams) {
   if (!testValue) console.warn(message, ...optionalParams);
 }
