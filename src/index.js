3f3aa255c69fb3a16e4a242368e9171047d67f57
Artem Zakharchenko <kettanaito@gmail.com>
2018-02-20 17:12:34 +0100

    Update package dependencies

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ export createField from './components/createField';
 /* Components */
 export FormProvider from './components/FormProvider';
 export Form from './components/Form';
-export Condition from './components/Condition';
 export * as Field from './components/Field';
+export Condition from './components/Condition';
 
 /* Field presets */
 export * as fieldPresets from './fieldPresets';
