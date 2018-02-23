b543a18f1b42229880a94b5235a2d441f5a70002
Artem Zakharchenko <kettanaito@gmail.com>
2018-02-23 16:02:48 +0100

    createField: Remove "disabled" from the field record; Utils: Remove "getPropsPatch" method

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,7 +9,6 @@ export IterableInstance from './IterableInstance';
 /* React utils */
 export getComponentName from './getComponentName';
 export getProperty from './getProperty';
-export getPropsPatch from './getPropsPatch';
 
 /* Form utils */
 export getFormRules from './getFormRules';
