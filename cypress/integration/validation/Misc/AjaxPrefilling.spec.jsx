985a77872d86df3bc0fb3e9365153ef7a279e806
Artem Zakharchenko <kettanaito@gmail.com>
2018-02-23 16:58:20 +0100

    Integration: Add "Ajax prefilling for controlled fields" scenario

---
+++ cypress/integration/validation/Misc/AjaxPrefilling.spec.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { mount } from 'cypress-react-unit-test';
+import Scenario, { timeoutDuration } from '@scenarios/AjaxPrefilling';
+
+describe('AJAX Pre-filling', function () {
+  before(() => {
+    mount(<Scenario />);
+    cy.get('#ajax').click().wait(timeoutDuration);
+  });
+
+  it('Pre-fills value properly', () => {
+    cy.get('[name="street"]')
+      .should('have.value', 'Baker')
+      .should('have.class', 'valid');
+  });
+
+  it('Validates pre-filled value properly', () => {
+    cy.get('[name="streetRule"]')
+      .should('have.value', 'Baker')
+      .should('have.class', 'invalid');
+  });
+});
