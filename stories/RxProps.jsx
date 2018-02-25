af9f6620fb1de9224eee110240eb870d30a84f97
Artem Zakharchenko <kettanaito@gmail.com>
2018-02-25 10:39:02 +0100

    rxUtils (createSubscriptions): Prevent "dispatch" wrapper to allow proper Object Proxy

--- stories/RxProps.jsx
+++ stories/RxProps.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, FormProvider, Field } from '../src';
+import { FormProvider, Form, Field } from '../src';
 import { MyInput } from './custom-fields';
 
 const providerRules = {
@@ -44,45 +44,47 @@ export default class Messages extends React.Component {
     const { type } = this.state;
 
     return (
-      <Form
-        rules={ rules }
-        messages={ messages }
-        onSubmitStart={ this.handleSubmitStart }>
-        <h2>Reactive props</h2>
+      <FormProvider withImmutable>
+        <Form
+          rules={ rules }
+          messages={ messages }
+          onSubmitStart={ this.handleSubmitStart }>
+          <h2>Reactive props</h2>
 
-        <MyInput
-          name="fieldOne"
-          initialValue="foo" />
+          <MyInput
+            name="fieldOne"
+            initialValue="foo" />
 
-        <MyInput
-          name="rxField"
-          required={({ fields }) => {
-            const conditionA = fields.fieldOne && !!fields.fieldOne.value;
-            const conditionB = fields.fieldThree && (fields.fieldThree.type === 'password');
+          <MyInput
+            name="rxField"
+            required={({ fields }) => {
+              const conditionA = fields.fieldOne && !!fields.fieldOne.value;
+              const conditionB = fields.fieldThree && (fields.fieldThree.type === 'password');
 
-            return conditionA && conditionB;
-          }} />
+              return conditionA && conditionB;
+            }} />
 
-        <MyInput
-          name="fieldThree"
-          type={ type }
-          initialValue="something" />
+          <MyInput
+            name="fieldThree"
+            type={ type }
+            initialValue="something" />
 
-        {/* <MyInput
-          name="fieldThree"
-          required={({ fields, form }) => {
-            return form.subscribe('fieldTwo', 'value', ({ value }) => !!value);
-          }} /> */}
+          {/* <MyInput
+            name="fieldThree"
+            required={({ fields, form }) => {
+              return form.subscribe('fieldTwo', 'value', ({ value }) => !!value);
+            }} /> */}
 
-        <button onClick={(event) => {
-          event.preventDefault();
-          this.setState(({ type }) => ({
-            type: (type === 'text') ? 'password' : 'text'
-          }));
-        }}>Toggle type</button>
+          <button onClick={(event) => {
+            event.preventDefault();
+            this.setState(({ type }) => ({
+              type: (type === 'text') ? 'password' : 'text'
+            }));
+          }}>Toggle type</button>
 
-        <button>Submit</button>
-      </Form>
+          <button>Submit</button>
+        </Form>
+      </FormProvider>
     );
   }
 }
