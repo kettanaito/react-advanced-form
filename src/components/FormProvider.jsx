e14c3eec460061c5d006bc9e59450ea8b7e2a43d
Artem Zakharchenko <kettanaito@gmail.com>
2018-03-01 16:01:34 +0100

    Form: Use a fallback value for "this.context.debounceTime" when not provided

--- src/components/FormProvider.jsx
+++ src/components/FormProvider.jsx
@@ -7,6 +7,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { fromJS } from 'immutable';
 
+export const defaultDebounceTime = 250;
+
 export const TValidationRules = PropTypes.shape({
   type: PropTypes.object, // type-specific field validation rules
   name: PropTypes.object // name-specific field validation rules
@@ -23,20 +25,20 @@ export default class FormProvider extends React.Component {
     rules: TValidationRules,
     messages: TValidationMessages,
     withImmutable: PropTypes.bool,
-    debounceTime: PropTypes.number.isRequired
+    debounceTime: PropTypes.number
   }
 
   static defaultProps = {
     messages: {},
     withImmutable: false,
-    debounceTime: 250
+    debounceTime: defaultDebounceTime
   }
 
   static childContextTypes = {
     rules: TValidationRules,
     messages: TValidationMessages,
     withImmutable: PropTypes.bool,
-    debounceTime: PropTypes.number.isRequired
+    debounceTime: PropTypes.number
   }
 
   getChildContext() {
