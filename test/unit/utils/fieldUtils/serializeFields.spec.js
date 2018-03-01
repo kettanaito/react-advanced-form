ac31b38fffb70dd467255a60702125c4af5bc78f
Artem Zakharchenko <kettanaito@gmail.com>
2018-03-01 14:02:02 +0100

    Form: Refine validation and serialization to work with nested field keys

--- test/unit/utils/fieldUtils/serializeFields.spec.js
+++ test/unit/utils/fieldUtils/serializeFields.spec.js
@@ -5,12 +5,12 @@ import { fieldUtils } from '../../../../src/utils';
 describe('serializeFields', function () {
   it('Serializes different field types properly', () => {
     const fields = fromJS({
-      text: { fieldPath: 'text', value: 'text', valuePropName: 'value' },
-      textEmpty: { fieldPath: 'fieldTwo', value: '', valuePropName: 'value' },
-      checkbox: { fieldPath: 'checkbox', type: 'checkbox', checked: false, valuePropName: 'checked' },
-      select: { fieldPath: 'select', type: 'select', value: 'select', valuePropName: 'value' },
-      textarea: { fieldPath: 'textarea', type: 'textarea', value: 'textarea', valuePropName: 'value' },
-      radio: { fieldPath: 'radio', type: 'radio', value: 'radio', valuePropName: 'value' }
+      text: { fieldPath: ['text'], value: 'text', valuePropName: 'value' },
+      textEmpty: { fieldPath: ['fieldTwo'], value: '', valuePropName: 'value' },
+      checkbox: { fieldPath: ['checkbox'], type: 'checkbox', checked: false, valuePropName: 'checked' },
+      select: { fieldPath: ['select'], type: 'select', value: 'select', valuePropName: 'value' },
+      textarea: { fieldPath: ['textarea'], type: 'textarea', value: 'textarea', valuePropName: 'value' },
+      radio: { fieldPath: ['radio'], type: 'radio', value: 'radio', valuePropName: 'value' }
     });
 
     const serialized = fieldUtils.serializeFields(fields).toJS();
@@ -26,11 +26,11 @@ describe('serializeFields', function () {
 
   it('Serializes groupped fields properly', () => {
     const fields = fromJS({
-      fieldOne: { fieldPath: 'fieldOne', value: 'one', valuePropName: 'value' },
-      fieldTwo: { fieldPath: 'groupOne.fieldTwo', value: 'two', valuePropName: 'value' },
-      fieldThree: { fieldPath: 'fieldThree', value: 'three', valuePropName: 'value' },
-      fieldFour: { fieldPath: 'groupOne.fieldFour', value: 'four', valuePropName: 'value' },
-      fieldFive: { fieldPath: 'groupTwo.fieldFive', value: 'five', valuePropName: 'value' },
+      fieldOne: { fieldPath: ['fieldOne'], value: 'one', valuePropName: 'value' },
+      fieldTwo: { fieldPath: ['groupOne', 'fieldTwo'], value: 'two', valuePropName: 'value' },
+      fieldThree: { fieldPath: ['fieldThree'], value: 'three', valuePropName: 'value' },
+      fieldFour: { fieldPath: ['groupOne', 'fieldFour'], value: 'four', valuePropName: 'value' },
+      fieldFive: { fieldPath: ['groupTwo', 'fieldFive'], value: 'five', valuePropName: 'value' },
     });
 
     const serialized = fieldUtils.serializeFields(fields).toJS();
@@ -50,9 +50,9 @@ describe('serializeFields', function () {
 
   it('Bypass the fields marked as "skip"', () => {
     const fields = fromJS({
-      fieldOne: { fieldPath: 'fieldOne', value: 'foo', valuePropName: 'value' },
-      fieldTwo: { fieldPath: 'fieldTwo', value: 'skipped', valuePropName: 'value', skip: true },
-      fieldThree: { fieldPath: 'fieldThree', value: '123', valuePropName: 'value' }
+      fieldOne: { fieldPath: ['fieldOne'], value: 'foo', valuePropName: 'value' },
+      fieldTwo: { fieldPath: ['fieldTwo'], value: 'skipped', valuePropName: 'value', skip: true },
+      fieldThree: { fieldPath: ['fieldThree'], value: '123', valuePropName: 'value' }
     });
 
     const serialized = fieldUtils.serializeFields(fields).toJS();
