1da1a704c33db7a58c81a006fa41f054eff89461
Artem Zakharchenko <kettanaito@gmail.com>
2018-03-15 20:40:49 +0100

    Removes unused integration configuration files

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,16 +1,3 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
 const path = require('path');
 const webpack = require('@cypress/webpack-preprocessor');
 
@@ -47,7 +34,5 @@ const options = {
 };
 
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
   on('file:preprocessor', webpack(options))
-}
+};
