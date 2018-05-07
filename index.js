0284c11dadf30bf82e10cdb2da3e4c37f70e9204
Artem Zakharchenko <kettanaito@gmail.com>
2018-05-07 16:15:18 +0200

    Ships library in "production" mode by default

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./lib/index.production.js');
-} else {
+if (process.env.NODE_ENV === 'development') {
   module.exports = require('./lib/index.development.js');
+} else {
+  module.exports = require('./lib/index.production.js');
 }
