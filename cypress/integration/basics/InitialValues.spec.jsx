f4da3682a81b19ba665712bfcee65a9c7e8553a3
Artem Zakharchenko <kettanaito@gmail.com>
2018-08-28 16:50:00 +0200

    Tests initial value propagation for grouped fields

--- cypress/integration/basics/InitialValues.spec.jsx
+++ cypress/integration/basics/InitialValues.spec.jsx
@@ -6,6 +6,9 @@ describe('Initial values', function() {
   const initialValues = {
     firstName: 'Kate',
     username: 'admin',
+    billingAddress: {
+      street: 'Baker st.',
+    },
   }
 
   before(() => {
@@ -14,12 +17,18 @@ describe('Initial values', function() {
 
   it('Takes "fieldProps.initialValue" as the highest priority', () => {
     cy.getField('firstName').should('have.value', 'John')
+    cy.getField('houseNumber').should('have.value', '4')
   })
 
-  it('Takes "Form.props.initialValues" the highest priority source of initial value', () => {
+  it('Takes "Form.props.initialValues"', () => {
     cy.getField('username')
       .should('have.value', initialValues.username)
       .valid()
+
+    cy.getField('street').should(
+      'have.value',
+      initialValues.billingAddress.street,
+    )
   })
 
   it('Takes field-wide "initialValue" as the last fallback, when such value is set', () => {
