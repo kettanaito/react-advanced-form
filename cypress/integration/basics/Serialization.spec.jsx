bea5bc4b46bbd7cb3904f1bd8995a2b60f95f3f6
Artem Zakharchenko <kettanaito@gmail.com>
2018-08-30 21:44:19 +0200

    Adds integration test cases for "Serialized" transformations

---
+++ cypress/integration/basics/Serialization.spec.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { expect } from 'chai'
+import Scenario, { formatAddress } from '@examples/basics/Serialize'
+
+describe('Serialization', function() {
+  before(() => {
+    cy.loadStory(<Scenario getRef={(form) => (this.form = form)} />)
+  })
+
+  const submit = () => cy.get('button[type="submit"]').click()
+
+  it('Applies custom serialized fields transformer, when specified', () => {
+    cy.get('form').should(() => {
+      const { fields } = this.form.state
+      const { street, houseNumber } = formatAddress(fields.billingAddress.address.value)
+      const serialized = this.form.serialize()
+
+      expect(serialized).to.deep.equal({
+        email: fields.email.value,
+        password: btoa(fields.password.value),
+        billingAddress: {
+          street,
+          houseNumber,
+        },
+      })
+    })
+  })
+})
