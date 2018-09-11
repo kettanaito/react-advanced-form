c3ae4ae13b39fc4c01d3b222c74542e44e6a2441
Artem Zakharchenko <kettanaito@gmail.com>
2018-09-11 20:58:57 +0200

    Replaces "stitchBy" with "stitchWith" function

--- src/utils/fieldUtils/index.js
+++ src/utils/fieldUtils/index.js
@@ -1,4 +1,4 @@
-/* Other */
 export serializeFields from './serializeFields'
 export createPropGetter from './createPropGetter'
 export flattenFields from './flattenFields'
+export stitchFields from './stitchFields'
---
+++ src/utils/stitchWith.test.js
@@ -0,0 +1,78 @@
+import * as R from 'ramda'
+import stitchWith from './stitchWith'
+
+test('Stitches the given list into an object', () => {
+  const fieldOne = {
+    name: 'fieldOne',
+    fieldPath: ['fieldOne'],
+    value: 1,
+  }
+
+  const fieldTwo = {
+    name: 'fieldTwo',
+    fieldPath: ['groupName', 'fieldTwo'],
+    value: 2,
+  }
+
+  const fieldThree = {
+    name: 'fieldTwo',
+    fieldPath: ['groupName', 'fieldThree'],
+    value: 2,
+  }
+
+  const res = stitchWith(
+    (a) => a,
+    ['fieldPath'],
+    [fieldOne, fieldTwo, fieldThree],
+  )
+
+  expect(res).toEqual({
+    fieldOne,
+    groupName: {
+      fieldTwo,
+      fieldThree,
+    },
+  })
+})
+
+test('Stitches the given list using custom needle function', () => {
+  const res = stitchWith(
+    (entry, keyPath, acc) => R.append(entry, R.pathOr([], keyPath, acc)),
+    ['k'],
+    [
+      {
+        k: 'a',
+        v: 1,
+      },
+      {
+        k: ['b', 'c'],
+        v: 2,
+      },
+      {
+        k: ['b', 'c'],
+        v: 3,
+      },
+    ],
+  )
+
+  expect(res).toEqual({
+    a: [
+      {
+        k: 'a',
+        v: 1,
+      },
+    ],
+    b: {
+      c: [
+        {
+          k: ['b', 'c'],
+          v: 2,
+        },
+        {
+          k: ['b', 'c'],
+          v: 3,
+        },
+      ],
+    },
+  })
+})
