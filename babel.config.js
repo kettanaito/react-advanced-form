36f7c21b38254eec057932f078674d011caca408
Artem Zakharchenko <kettanaito@gmail.com>
2018-09-15 21:14:52 +0200

    Fixes jest running with babel@7

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+'use strict'
+
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
+  plugins: [
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+  ],
+  env: {
+    production: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+          },
+        ],
+        '@babel/preset-react',
+        '@babel/preset-flow',
+      ],
+    },
+  },
+}
