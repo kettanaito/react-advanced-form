972f30a7e74008403c5b655786d2884139aed87c
Artem Zakharchenko <kettanaito@gmail.com>
2018-09-27 12:01:53 +0200

    Tree shakes Ramda modules via "babel-plugin-ramda"

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
     '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
+    'babel-plugin-ramda',
   ],
   env: {
     production: {
