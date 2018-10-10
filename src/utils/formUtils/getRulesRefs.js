b6459f09978b32f2252d1abbdef709a931eaa18f
Artem Zakharchenko <kettanaito@gmail.com>
2018-10-10 12:23:51 +0200

    Curries "getRulesRefs"

--- src/utils/formUtils/getRulesRefs.js
+++ src/utils/formUtils/getRulesRefs.js
@@ -7,14 +7,13 @@ import flushFieldRefs from '../flushFieldRefs'
  * Iterates over the list of field rules, flushes fields references
  * from each resolver function and adds "refs" property to each rule.
  */
-const getRulesRefs = (
-  resolverArgs: Object,
-  resolverRecords: ResolverRecord[],
-): Object[] => {
-  return R.map((rule) => {
-    const { refs } = flushFieldRefs(rule.resolver, resolverArgs)
-    return R.assoc('refs', refs, rule)
-  })(resolverRecords)
-}
+const getRulesRefs = R.curry(
+  (resolverArgs: Object, resolverRecords: ResolverRecord[]): Object[] => {
+    return R.map((rule) => {
+      const { refs } = flushFieldRefs(rule.resolver, resolverArgs)
+      return R.assoc('refs', refs, rule)
+    })(resolverRecords)
+  },
+)
 
 export default getRulesRefs
