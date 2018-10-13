98f1027b0c3174b6048f724e6aa07c3d8ea2f5da
Artem Zakharchenko <kettanaito@gmail.com>
2018-10-13 10:38:14 +0200

    Adds "touched" property on the field record

--- src/utils/recordUtils.js
+++ src/utils/recordUtils.js
@@ -28,6 +28,7 @@ export const createField = (initialState) => {
     [valuePropName]: value,
     valuePropName,
     focused: false,
+    touched: false,
     skip: false,
 
     /* Validation */
@@ -49,7 +50,7 @@ export const createField = (initialState) => {
 
     reactiveProps: null,
 
-    /* Event callbacks/handlers */
+    /* Event handlers */
     onFocus: null,
     onChange: null,
     onBlur: null,
@@ -189,6 +190,7 @@ export const reset = R.curry((nextValueGetter, fieldProps) => {
     // Beware that this will set value to "undefined" when no "initialValue" is found
     setValue(nextValueGetter(fieldProps)),
     setErrors(null),
+    setTouched(false),
     resetValidationState,
     resetValidityState,
   )(fieldProps)
@@ -201,3 +203,5 @@ export const reset = R.curry((nextValueGetter, fieldProps) => {
  * @returns {Object}
  */
 export const setFocus = R.assoc('focused')
+
+export const setTouched = R.assoc('touched')
