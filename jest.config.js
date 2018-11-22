00b260e4e7444da431bfccce1047b22f6b4beeed
Artem Zakharchenko <kettanaito@gmail.com>
2018-11-22 21:59:29 +0100

    Sets "roots" for jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   name: 'react-advanced-form',
   verbose: true,
-  testMatch: [
-    '<rootDir>/src/utils/**/*.test.{js,jsx}',
-    '<rootDir>/src/utils/validation/**/*.test.{js,jsx}',
-  ],
+  roots: ['src/utils'],
 }
